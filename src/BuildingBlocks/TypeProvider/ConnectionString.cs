43953bc93c0e5bc6a4fe43f5e603e7eb42a35be5
kalepook <kooroosh.kabir1@gmail.com>
2023-10-28 14:09:48 +0330

    feat : added a project as TypeProvider As the name is shown you the project provides some types to use read the README of the project for more

---
+++ src/BuildingBlocks/TypeProvider/ConnectionString.cs
@@ -0,0 +1,21 @@
+﻿namespace ShopBasedOnMs.BuildingBlocks.TypeProvider;
+
+public readonly record struct ConnectionString
+{
+    private readonly string Value;
+
+    public ConnectionString(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException("The value can't be null or empty as connection string", nameof(value));
+        }
+
+        Value = value;
+    }
+
+    public override string ToString() => Value;
+
+    public static implicit operator string(ConnectionString conn) => conn.Value;
+    public static explicit operator ConnectionString(string value) => new(value);
+}
